18a1f9869dcc1ae9be1cd37ba0d87cd6a6017b94
Rohan Gnanaolivu <rohandavidg@gmail.com>
2015-12-16 11:47:32 -0600

    added more code to import

--- import_to_access.py
+++ import_to_access.py
@@ -33,13 +33,22 @@ def run(excel_workbook, logger_filename):
     import_carrier_id_table = connect_to_access(carrier_id_sheet)
 
     
-def connect_to_access(sheet):
+def connect_to_access(carrier_id_sheet, caid_cast_sheet, cast_plate_sheet):
     DBfile = 'c:\\Users\m149947\Desktop\couch\CARRIERS\database\CARRIERS_SubManifestOnly.accdb'
     conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile)
     cursor = conn.cursor()
+    carriers_id_table = import_carriers_id_table(cursor, carrier_id_sheet)
+    caid_cast_table = import_caid_cast_table(cursor, caid_cast_sheet)
+    cast_plate_table = import_cast_plate(cursor, cast_plate_sheet)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    
+def import_carriers_id_table(cursor, sheet):
     SQL = """
-INSERT INTO "CARRIERS ID" ([CARRIERS ID], [Sub_Sample Name], [Sub_Individual ID], Sub_Gender, [Sub_Sample Status], Sub_Pedigree, [Sub_Mother ID], [Sub_Father ID], [Sub_Disease Type], Sub_Race, Sub_Ethnicity, [CARRIERS ID Comment]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-"""
+    INSERT INTO "CARRIERS ID" ([CARRIERS ID], [Sub_Sample Name], [Sub_Individual ID], [Sub_Gender], [Sub_Sample Status], [Sub_Pedigree], [Sub_Mother ID], [Sub_Father ID], [Sub_Disease Type], [Sub_Race], [Sub_Ethnicity], [CARRIERS ID Comment]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
 
     for r in xrange(1, sheet.nrows):
         CARRIERS_ID = sheet.cell(r,0).value
@@ -58,9 +67,49 @@ INSERT INTO "CARRIERS ID" ([CARRIERS ID], [Sub_Sample Name], [Sub_Individual ID]
         values = (CARRIERS_ID, Sub_Sample_Name, Sub_Individual_ID, Sub_Gender, Sub_Sample_Status, Sub_Pedigree, Sub_Mother_ID, Sub_Father_ID, Sub_Disease_Type, Sub_Race, Sub_Ethnicity, CARRIERS_ID_Comment)
         
         cursor.execute(SQL, values)
+
+def import_caid_cast_table(cursor, sheet):
+    SQL =  """
+    INSERT INTO "CAID_CAST" ([CAID_CAST_KEY], [CARRIERS ID], [CAST Barcode], [Sub_Coord], [Sub_Vol], [Sub_Conc], [Sub_Alias], [Sub_Site of Origin], [Sub_Tissue Source], [Sub_Sample Blank], [Sub_Comment], [CAID_CAST Comment]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
+    for r in xrange(1, sheet.nrows):
+        CAID_CAST_KEY = sheet.cell(r,0).value
+        CARRIERS_ID = sheet.cell(r,1).value
+        CAST_BARCODE = sheet.cell(r,2).value
+        Sub_Coord = sheet.cell(r,3).value
+        Sub_Vol = sheet.cell(r,4).value
+        Sub_Conc = sheet.cell(r,5).value
+        Sub_Alias = sheet.cell(r,6).value
+        Sub_Site_of_origin = sheet.cell(r,7).value
+        Sub_Tissue_Source = sheet.cell(r,8).value
+        Sub_Sample_Blank = sheet.cell(r,9).value
+        Sub_Comment = sheet.cell(r,10).value
+        CAID_CAST_Comment = sheet.cell(r,11).value
         
-    cursor.close()
-    conn.close()
+        values = (CAID_CAST_KEY, CARRIERS_ID, CAST_BARCODE, Sub_Coord, Sub_Vol, Sub_Conc, Sub_Alias, Sub_Site_of_origin, Sub_Tissue_Source, Sub_Sample_Blank, Sub_Comment, CAID_CAST_Comment)
+
+        cursor.execute(SQL, values)
+
+def import_cast_plate(cursor, sheet):
+    SQL = """
+    INSERT INTO "CAST Plate" ([CAST Barcode], [CAST Plate/Box], [Date Received], [Sub_Contact ID], [Sub_Contact_Person],[Sub_Contact E-mail], [Sub_Project Type], [Sub_Plate Name], [Sub_Plate Description],[CAST Plate Location], [CAST Plate comment]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
+
+    for r in xrange(1, sheet.nrows):
+        CAST_Barcode = sheet.cell(r,0).value
+        CAST_plate_box = sheet.cell(r,1).value
+        Date_received = sheet.cell(r,2).value
+        sub_contact_id = sheet.cell(r,3).value
+        sub_contact_person = sheet.cell(r,4).value
+        sub_contact_email = sheet.cell(r,5).value
+        sub_project_type = sheet.cell(r,6).value
+        sub_plate_name = sheet.cell(r,7).value
+        sub_plate_description = sheet.cell(r,8).value
+        cast_plate_location = sheet.cell(r,9).value
+        cast_plate_comment = sheet.cell(r,10).value
+
+        values = (CAST_Barcode, CAST_plate_box, Date_received, sub_contact_id, sub_contact_person, sub_project_type, sub_plate_name, sub_plate_description, cast_plate_location, cast_plate_comment)
+        cursor.execute(SQL, values)
     
 if __name__ == '__main__':
     main()
